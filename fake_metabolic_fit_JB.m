47bcc7929f4c5427127ce85255ea6fa8d6538803
Greg Orekhov <go226@nau.edu>
2019-03-26 17:15:23 -0700

    Updated Fake_Metabolic_Fit_JB to support 1 and 2 parameter optimization

--- fake_metabolic_fit_JB.m
+++ fake_metabolic_fit_JB.m
@@ -1,8 +1,15 @@
-function [SS, y_bar]=fake_metabolic_fit_JB(ParamsForCondition);
+function [SS, y_bar]=fake_metabolic_fit_JB(ParamsForCondition)
 
-x=ParamsForCondition(1);
-%y=ParamsForCondition(2);
-cost=(x-12)^2;%+(y-67)^2;
+if length(ParamsForCondition)==2    
+    x=ParamsForCondition(1);
+    y=ParamsForCondition(2);
+    cost=(x-12)^2+(y-67)^2;
+elseif length(ParamsForCondition)==1
+    x=ParamsForCondition(1);
+    cost=(x-12)^2;
+else
+    error("Number of parameters defined not compatible with current script.");
+end
 SS=cost;
 y_bar=linspace(0,10);
 
