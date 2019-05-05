2299b1a67a2b2e116dbddfaa2038ba4978f53f56
Greg <go226@nau.edu>
2019-05-04 19:28:38 -0700

    Fixed some issues after merging

--- Optimization.m
+++ Optimization.m
@@ -715,9 +715,11 @@ else
                              end
                             tic %reinitialize timer once you start next controller
                             %nonzeros(breathtimes) %uncomment if you want to see that its working
-                            [SS, y_bar]=fake_metabolic_fit_JB(ParamsForCondition,AverageLapSpeed); %Used for testing the optimization
+                            %[SS, y_bar]=fake_metabolic_fit_JB(ParamsForCondition,AverageLapSpeed); %Used for testing the optimization
+                            [SS,y_bar] = metabolic_average(fullrate,breathtimes,AverageLapSpeed);
                             Full_y_bar{ConditionNumber} = y_bar';
-                            Full_Metabolic_Data_to_Save{ConditionNumber} = [y_bar',10*y_bar'];
+                            %Full_Metabolic_Data_to_Save{ConditionNumber} = [y_bar',10*y_bar'];
+                            Full_Metabolic_Data_to_Save{ConditionNumber} = [nonzeros(fullrate),nozeros(breathtimes)];
                             SSdata(ConditionNumber, :) = [SS, ConditionNumber, ParamsForCondition]; 
                             ConditionNumber = ConditionNumber+1;
                             counteval = counteval+1; %Keeps track of how many conditions have been tested. 
